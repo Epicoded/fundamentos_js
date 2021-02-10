8027e78036b22446be14f3665a5305efb01ff56b
Perez Carreño, Vicente Eduardo <vicenteeduardo.perez@axa.com.mx>
2021-02-09 19:10:21 -0600

    clase 1 a 3 laptop 09/02/2021

---
+++ clase1.js
@@ -0,0 +1,6 @@
+var nombre ,apellido = 'Lifszyc';
+nombre = 'Sacha';
+var edad= 28;
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad + ' años');
---
+++ clase2.js
@@ -0,0 +1,14 @@
+var nombre= 'Eduardo' ,apellido = 'Perez';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = nombre.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = nombre +' '+ apellido;
+var nombreCompleto2 = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.substr(1,2);
+
+var lastWord = nombre.charAt(nombre.length - 1);
