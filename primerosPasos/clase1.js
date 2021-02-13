a3f7ddc98a67a4a115e28d6b4a2f2ac2424b2e83
Gexed <gexone@gmail.com>
2021-02-12 22:08:38 -0600

    arreglo de carpetas

--- primerosPasos/clase1.js
+++
@@ -1,6 +0,0 @@
-var nombre ,apellido = 'Lifszyc'; //formas de declaración y de inicializacion de variables
-nombre = 'Sacha';
-var edad= 28;
-
-console.log('Hola ' + nombre + ' ' + apellido);
-console.log('Tengo ' + edad + ' años');
